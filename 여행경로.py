bb74cc7e88e8e571e5f0c178b9595c6a73016248
Chae Lyn Jang <59859464+soodal5629@users.noreply.github.com>
2021-12-27 15:17:19 +0900

    Create 여행경로.py
    프로그래머스 dfs 문제
    - 파이썬 dictionary 이용 -> defaultdict(list)

---
+++ 여행경로.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+def solution(tickets):
+    answer = []
+    routes = defaultdict(list)
+    for key, value in tickets:
+        routes[key].append(value)
+    for key, value in tickets:
+        routes[key].sort(reverse = True)
+    stack = ["ICN"]
+    while stack:
+        temp = stack[-1]
+        if routes[temp]:
+            stack.append(routes[temp].pop())
+        else:
+            answer.append(stack.pop())
+    answer.reverse()
+    return answer
