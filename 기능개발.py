0cec83812d96b3cb6e4e8ec3b6bd9813f5fc3a55
Chae Lyn Jang <59859464+soodal5629@users.noreply.github.com>
2022-01-07 09:53:36 +0900

    Create 기능개발.py
    프로그래머스 스택/큐 문제
    - 테스트케이스 한번에 통과 못해서 반례 케이스 보고 통과

---
+++ 기능개발.py
@@ -0,0 +1,25 @@
+import math
+from collections import deque
+def solution(progresses, speeds):
+    answer = []
+    time = deque()
+    for i in range(len(progresses)):
+        temp = math.ceil((100 - progresses[i])/speeds[i])
+        time.append(temp)
+    q = deque()
+    if len(time)==1:
+        answer.append(time[0])
+    else:
+        q.append(time.popleft())
+        while time:
+            stack_bottom = q[0]
+            push = time.popleft()
+            if stack_bottom >= push:
+                q.append(push)
+            else:
+                answer.append(len(q))
+                q.clear()
+                q.append(push)
+        if len(q)>0:
+            answer.append(len(q))
+    return answer
