20206a89bcbd6224daa8ed3e942386162b59e96c
Chae Lyn Jang <59859464+soodal5629@users.noreply.github.com>
2022-01-19 17:18:00 +0900

    Create 주식가격.py
    프로그래머스 스택/큐 문제
    - 승현이 해설 듣고 해결,, 괜히 골치아팠음

---
+++ 주식가격.py
@@ -0,0 +1,14 @@
+from collections import deque
+def solution(prices):
+    answer = []
+    q = deque(prices)
+    while q:
+        now = q.popleft()
+        flag = False
+        cnt = 0
+        for i in q:
+            cnt +=1
+            if now > i:
+                break
+        answer.append(cnt)
+    return answer
