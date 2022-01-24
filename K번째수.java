d6326ef2eb510f34e77b959738dacb57441f7c4a
Chae Lyn Jang <59859464+soodal5629@users.noreply.github.com>
2022-01-24 10:23:02 +0900

    Create K번째수.java
    프로그래머스 - 정렬문제

---
+++ K번째수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+   public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for(int i=0; i<commands.length;i++){
+            int start = commands[i][0];
+            int end = commands[i][1];
+            int k = commands[i][2];
+            int [] temp = Arrays.copyOfRange(array, start-1, end);
+            Arrays.sort(temp);
+            answer[i] = temp[k-1];
+        }
+    return answer;
+   }
+}
