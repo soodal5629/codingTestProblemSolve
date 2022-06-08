ae789c03e59d356fa9372d4d2197d8aff3d891ad
Chae Lyn Jang <59859464+soodal5629@users.noreply.github.com>
2022-06-08 13:53:42 +0900

    Create 행렬의 곱셈.py
    프로그래머스 레벨2

---
+++ 행렬의 곱셈.py
@@ -0,0 +1,12 @@
+def solution(arr1, arr2):
+    answer = []
+    for i in range(len(arr1)):
+        temp = []
+        for j in range(len(arr2[0])):
+            sum = 0 
+            for k in range(len(arr2)):
+                sum += (arr1[i][k] * arr2[k][j])
+            temp.append(sum)
+        answer.append(temp)
+    
+    return answer
