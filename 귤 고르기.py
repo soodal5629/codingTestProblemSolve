516e255436a49b360f6ffa5cf9c2b56229185e02
ChaeLyn Jang <59859464+soodal5629@users.noreply.github.com>
2022-11-30 14:47:05 +0900

    Create 귤 고르기.py
    프로그래머스 레벨2
    내가 직접 set과 정렬을 이용해서 푼 방식은 시간초과 남
    위에 Counter 이용한 것은 velog 에서 따온 코드,,

---
+++ 귤 고르기.py
@@ -0,0 +1,10 @@
+from collections import Counter
+def solution(k, tangerine):
+    answer = 0
+    d = sorted(Counter(tangerine).items(),reverse = True, key = lambda x : x[1])
+    for key, value in d:
+        if k <= 0: break
+        k -= value
+        answer += 1
+        
+    return answer
