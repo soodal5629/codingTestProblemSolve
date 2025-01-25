e80cbefa06b861de12e30172441d5c09f0b85c50
ChaeLyn Jang <59859464+soodal5629@users.noreply.github.com>
2025-01-25 16:31:40 +0900

    Create 백준13700(완전 범죄).py
    그래프 문제 silver1
    pypy3로 제출해야 시간초과 안나고 통과한다 ㅜㅜ

---
+++ 백준13700(완전 범죄).py
@@ -0,0 +1,24 @@
+from collections import deque
+n, s, goal, f, b, k = map(int, input().split())
+p = []
+if k > 0: p = list(map(int, input().split()))
+d = deque()
+d.append((s, 0))
+v = [False for _ in range(n+f+1)]
+res = 1e9
+while d:
+  now, cnt = d.popleft()
+  v[now] = True
+  if now == goal:
+    res = min(res, cnt)
+    break
+  x = now+f
+  y = now-b
+  if  0 < x <= n and not v[x] and x not in p:
+    v[x] = True
+    d.append((x, cnt+1))
+  if 0 < y <= n and not v[y] and y not in p:
+    v[y]=True
+    d.append((y, cnt+1))
+if res != 1e9: print(res)
+else: print('BUG FOUND')
